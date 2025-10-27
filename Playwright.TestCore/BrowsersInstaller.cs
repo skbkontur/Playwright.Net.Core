411c2f9cf97656fff78b21aa2d47b7c1b483efe6
simonov.mn <simonov.mn@skbkontur.ru>
2025-10-27 15:26:12 +0500

    Подтюнил BrowsersInstaller

---
+++ Playwright.TestCore/BrowsersInstaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Playwright;
+
+namespace Kontur.Playwright.TestCore;
+
+public class BrowsersInstaller
+{
+    public void Install(params string[] browsers)
+    {
+        List<string> installArgs = ["install", "--with-deps"];
+        installArgs.AddRange(browsers);
+        var exitCode = Program.Main(installArgs.ToArray());
+        if (exitCode != 0)
+            throw new Exception($"Playwright exited with code {exitCode}");
+    }
+}
